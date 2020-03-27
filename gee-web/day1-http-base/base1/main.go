b09fc43fc69a0efa821e666b47834f0adba55a60
xpFelix <1092702994@qq.com>
2020-03-27 14:31:08 +0800

    小修改

--- gee-web/day1-http-base/base1/main.go
+++ gee-web/day1-http-base/base1/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/",indexHandler)
+	http.HandleFunc("/hello",helloHandler)
+	log.Fatal(http.ListenAndServe(":80",nil))
 }
+
+//handler echoes r.URL.Path
+func indexHandler(w http.ResponseWriter,req *http.Request)  {
+	fmt.Fprintf(w,"URL.Path = %q\n",req.URL.Path)
+}
+
+//handler echoes r.URL.Header
+func helloHandler(w http.ResponseWriter,req *http.Request){
+	for k,v := range req.Header{
+		fmt.Fprintf(w,"Header[%q]\n",k,v)
+	}
+}
+
